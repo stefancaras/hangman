5cd5f20d0c81bad4b0bf868a941550f3dc6e3f6c
Stefan <stefan.caras@gmail.com>
2023-08-13 22:03:16 +0300

    improvements

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const $ = (query) => document.querySelector(query);
 
-let word = [..."cameleon"];
-let hidden = [..."_".repeat(word.length)];
-let lives = 5;
+const words = [
+  "cameleon",
+  "bumblebee",
+  "baboon",
+  "butterfly",
+  "dolphin",
+  "elephant",
+  "penguin",
+  "squirrel",
+  "hippopotamus",
+  "kangaroo",
+];
+const word = [...words[Math.floor(Math.random() * words.length)]];
+const hidden = [..."_".repeat(word.length)];
+let lives = 6;
+const meow = new Audio("./media/meow.mp3");
+const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="red" class="bi bi-heart-fill" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+  </svg>`;
+$(".hearts").innerHTML = svg.repeat(6);
 
 const replaceChars = (char) => {
   for (let i in word) {
@@ -32,6 +49,7 @@ const gameOver = () => {
     return true;
   } else if (lives === 0) {
     message(`Game over.\nThe word was "${word.join("")}".`, 0);
+    $(".cat").style.display = "none";
     return true;
   }
   return false;
@@ -47,7 +65,9 @@ $(".buttons").addEventListener("click", (e) => {
       if (!gameOver()) message("Good guess!");
     } else {
       lives -= 1;
-      $(".container").style.backgroundImage = `url("./cat/${lives}.jpg")`;
+      meow.play();
+      $(".hearts").lastElementChild.remove();
+      $(".rope").style.backgroundImage = `url("./media/${lives}.png")`;
       if (!gameOver()) message("Wrong guess.");
     }
   }
